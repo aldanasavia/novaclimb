c40f452f3609a1d9a9624a1c056fc1060e528af4
aldanasavia <aldana.savia@gmail.com>
2025-07-01 16:32:21 -0300

    Add files via upload
    agrego la base de la parte de nosotros, sin estilos ni estructura, solo informción.

---
+++ src/pages/QuienesSomos/QuienesSomos.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+const QuienesSomos = () => {
+
+
+    return (
+        <>
+        <h1>¿Quienes somos?</h1>
+        <p>Somos un equipo multidisciplinario que une técnicos, diseñadores y escaladores para crear productos que marcan la diferencia.</p>
+        <p>Estamos enfocados en  la seguridad, innovación, integridad y sostenibilidad de nuestros productos</p>
+        <h2>Conocé nuestro equipo:</h2>
+        <ul>
+            <li><a href="https://www.linkedin.com/in/camila-huilen/">Camila Garcia | Diseñadora Audiovisual</a>
+                <br></br>
+                Referente del contenido audiovisual y áreas creativas
+            </li>
+            <li>Sebastian Leyes | Diseñador industrial
+                <br></br>
+                Referente del material PoP, manuales y diseño de packaging
+            </li>
+            <li><a href="https://www.linkedin.com/in/ignacio-nowosad-219028342/">Ignacio Nowosad | Técnico Electromecánico</a>
+                <br></br>
+                Gestión integral del proyecto NovaClimb, desarrollo de productos
+            </li>
+        </ul>
+        </>
+    )
+}
+
+export default QuienesSomos
