905232b7f6421049059feb9767a680e68bf66b7b
aldanasavia <aldana.savia@gmail.com>
2025-07-01 16:27:39 -0300

    Update App.jsx
    Agrega la Landing Page como lo primero que se ve y declara rutas de landing page y quienes somos

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
 import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LandingPage from './pages/LandingPage/LandingPage';
+import QuienesSomos from './pages/QuienesSomos/QuienesSomos'
 import './App.css'
 
 function App() {
@@ -6,7 +9,13 @@ function App() {
 
   return (
     <>
-     
+    <Router>
+        <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/nosotros" element = {<QuienesSomos />} />
+        </Routes>
+    </Router>
+  
     </>
   )
 }
